9a13d0ac46a78211849e33a7ddb853c8065bdc4f
RaphaelBensoussan <raphbens4@gmail.com>
2025-01-27 16:03:44 +0100

    test

--- src/test/java/com/sio/javatd5sio2/TodoListApplicationTest.java
+++ src/test/java/com/sio/javatd5sio2/TodoListApplicationTest.java
@@ -1,29 +1,29 @@
-//package com.sio.javatd5sio2;
-//
-//import javafx.scene.control.Alert;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.ListView;
-//import javafx.scene.control.TextField;
-//import javafx.stage.Stage;
-//import org.junit.jupiter.api.Test;
-//import org.testfx.api.FxRobot;
-//import org.testfx.framework.junit5.ApplicationTest;
-//
-//import java.io.IOException;
-//import java.util.Arrays;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class TodoListApplicationTest extends ApplicationTest {
-//
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        TodoListApplication app = new TodoListApplication();
-//        app.start(stage);
-//    }
-//
-//    // Add your tests here
-//
+package com.sio.javatd5sio2;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoListApplicationTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        TodoListApplication app = new TodoListApplication();
+        app.start(stage);
+    }
+
+    // Add your tests here
+
 //    @Test
 //    void testScenarioAddTask() {
 //        // Add your test here
@@ -45,4 +45,4 @@
 //        clickOn(addButton);
 //        assertTrue(lookup("Le titre ne peut pas être vide").tryQuery().isPresent());
 //    }
-//}
+}
